6d6662e5795c5c3c2ae546dd9f40fb518b88324b
Sydney Macdonald <macdonaldpsydney@gmail.com>
2022-07-05 18:22:42 -0700

    created ref and fetched program list

--- Components/Program.js
+++ Components/Program.js
@@ -3,6 +3,15 @@ import { Text, View, Button } from 'react-native';
 import { getDatabase, ref, get, child, val } from 'firebase/database';
 import db from '../firebase-config.js';
 
+const programsRef = ref(getDatabase(), 'programs');
+let programList = [];
+get(programsRef).then(snapshot => {
+  snapshot.forEach(item => {
+    const temp = item;
+    programList.push(temp);
+  })
+});
+
 const Programs = ({ navigation }) => {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
