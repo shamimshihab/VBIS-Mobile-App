0a696e4bde0979cac4be4f0b09f0b8ef66be129a
Sydney Macdonald <macdonaldpsydney@gmail.com>
2022-08-04 10:04:02 -0700

    added other resources code to firebase file

--- Database/firebase.js
+++ Database/firebase.js
@@ -34,14 +34,11 @@ get(child(aboutRef, "aboutVBIS")).then((snapshot) => {
 
 /*
 Staff
-Creates reference to staff child
-and creates list of staff members
+Creates list of staff members
 */
-const staffRef = ref(getDatabase(), "about/staff");
-
 let staffList = [];
 
-get(staffRef).then((snapshot) => {
+get(child(aboutRef, "staff")).then((snapshot) => {
   snapshot.forEach((item) => {
     const temp = item.val().split(":");
     staffList.push(temp);
@@ -120,4 +117,52 @@ get(programsRef).then((snapshot) => {
   });
 });
 
-export { about, staffList, address, email, hours, phone, programList };
+/*
+Other Resources
+Creates a reference to other resources child
+and two classes: a Resource Category and a Resource class.
+Gets a type for each category and gives it a list of services.
+Each service contains a name, description, location, 
+and phone number.
+*/
+const otherRef = ref(getDatabase(), "otherResources");
+
+class ResourceCategory {
+    constructor(type){
+        this.type = type;
+        this.serviceList = [];
+    }
+}
+
+class Resource {
+    constructor(name, description, location, phone){
+        this.name = name;
+        this.description = description;
+        this.location = location;
+        this.phone = phone;
+    }
+}
+
+let resourceDescription = "";
+let resourceCategoryList = [];
+
+get(otherRef).then((snapshot) => {
+    snapshot.forEach((item) => {
+        if(item.key == "description"){
+            resourceDescription = item.val();
+        }else{
+            const temp = new ResourceCategory(item.val().type);
+            item.forEach((service) => {
+                if(service.key != "type"){
+                    const tempService = new Resource(service.val().name, service.val().description, service.val().location, service.val().phone);
+                    temp.serviceList.push(tempService);
+                }
+            });
+            resourceCategoryList.push(temp);
+            console.log(temp.type);
+            console.log(temp.serviceList);
+        }
+    })
+})
+
+export { about, staffList, address, email, hours, phone, programList, resourceDescription, resourceCategoryList };
